5a0ed8312d0b5aefaa4d2baee6db4d2ed6ad660c
Daichi Aoki <fremie.off@gmail.com>
2024-05-17 21:42:22 +0900

    build: swift-http-types/dependencies を追加

--- Library/Package.swift
+++ Library/Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
   name: "Library",
+  platforms: [.iOS(.v17), .macOS(.v14)],
   // MEMO: [Library]以外で公開したい場合に定義する
   products: [
     .library(
@@ -13,9 +14,8 @@ let package = Package(
   ],
   // MEMO: [Library]内で使用する外部パッケージがある場合に定義する
   dependencies: [
-    //    .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.9.1"),
-    //    .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.2.0"),
-    //    .package(url: "https://github.com/zunda-pixel/LicenseProvider", from: "1.1.1"),
+    .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.3.0"),
+    .package(url: "https://github.com/apple/swift-http-types.git", from: "1.1.0"),
   ],
   // MEMO: 各パッケージ同士の依存関係を定義する
   targets: [
@@ -40,7 +40,13 @@ let package = Package(
       dependencies: ["Common"]),
     .target(
       name: "Network",
-      dependencies: ["Common", "Model"]),
+      dependencies: [
+        "Common",
+        "Model",
+        .product(name: "Dependencies", package: "swift-dependencies"),
+        .product(name: "HTTPTypes", package: "swift-http-types"),
+        .product(name: "HTTPTypesFoundation", package: "swift-http-types"),
+      ]),
     .target(
       name: "PokeSearchFeature",
       dependencies: [
@@ -48,8 +54,6 @@ let package = Package(
         "Designsystem",
         "Domain",
         "Model",
-        // MEMO: 外部パッケージを依存する場合、以下のように定義する
-        //        .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
       ]),
     .testTarget(
       name: "DomainTests",
